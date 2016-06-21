5ccd5df19f0d92913cdc773a583b5b768cf88385
dfan97 <davidfan219@gmail.com>
2016-06-21 11:08:44 -0400

    Added some more comments

--- phewas.R
+++ phewas.R
@@ -1,5 +1,4 @@
 library(RMySQL)
-library(dplyr)
 library(truncnorm)
 
 con <- dbConnect(MySQL(), user = "root", password = "root", dbname = "PheWAS", unix.sock="/Applications/MAMP/tmp/mysql/mysql.sock")
@@ -10,15 +9,16 @@ df <- dbReadTable(conn = con, name = "data")
 simulateComorbid <- function(n) {
   # one-coin flip for each comorbidity, rows of data frame are people
   # vectorized to remove for-loop
-  return(sapply(runif(n), function(x) rbinom(n = 5, size = 1,c(0.77, 0.3, 0.2, 0.3, 0.4)*(x>0.5) + c(0.57, 0.1, 0, 0.1, 0.2)*(x<0.5))))
+  return(sapply(runif(n), function(x) rbinom(n = 5, size = 1,c(0.77, 0.3, 0.2, 0.3, 0.4) * (x > 0.5) + c(0.57, 0.1, 0, 0.1, 0.2) * (x < 0.5))))
 }
 
 # hypertension(401), retinopathy(362.0), coronary heart disease(411), myocardial infarction(410), congestive heart failure(428)
-# vectorized
+# vectorized for speed
 simulateDiabeticCohort <- function(n) {
-  # transpose
-  mat <- as.data.frame(t(replicate(n, rbinom(n = nrow(df), size = 1, rtruncnorm(1, a = 0, b = 1, mean = 0, sd = 0.00015)))))
+  # transpose to n x nrow(df)
+  mat <- as.data.frame(t(replicate(n, rbinom(n = nrow(df), size = 1, rtruncnorm(1, a = 0, b = 1, mean = 0, sd = 0.0001)))))
   names(mat) <- paste("i", df$icd9, sep = "")
+  # everyone in the case group is a diabetic.
   mat$i250 <- 1
   comorbidities <- simulateComorbid(n)
   mat$i401 <- comorbidities[1,]
@@ -26,32 +26,33 @@ simulateDiabeticCohort <- function(n) {
   mat$i411 <- comorbidities[3,]
   mat$i410 <- comorbidities[4,]
   mat$i428 <- comorbidities[5,] 
-  
   return(mat)
 }
 
 
 diabICD9s <- simulateDiabeticCohort(1000)
-# apply mean() to columns (denoted by 2) to double-check probabilities add up right
+# apply mean() to columns (denoted by 2) to double-check that probabilities add up correctly
 apply(diabICD9s[ , c("i401", "i362.0", "i411", "i410", "i428")], 2, mean)
 
-# Control has no diabetics
+
+# Sample from normal distribution truncated to range 0 - 1, use as probabilty in a loaded coin flip
 simulateControlCohort <- function(n) {
-  # not-discrete so don't do Gaussian noises
-  mat <- as.data.frame(t(replicate(n, rbinom(n = nrow(df), size = 1, rtruncnorm(1, a = 0, b = 1, mean = 0, sd = 0.00015)))))
+  mat <- as.data.frame(t(replicate(n, rbinom(n = nrow(df), size = 1, rtruncnorm(1, a = 0, b = 1, mean = 0, sd = 0.0001)))))
   names(mat) <- paste("i", df$icd9, sep = "")
+  # all are non-diabetic
   mat$i250 <- 0
   return(mat)
 }
 
 normICD9s <- simulateControlCohort(1000)
-# 2 means to columns
+# ,2 means apply to columns
 apply(normICD9s[ , c("i401", "i362.0", "i411", "i410", "i428")], 2, mean)
+
 
 # Takes two data frames as parameters: rows are individuals and columns are ICD9 codes
 # performs unadjusted logistic regression and returns odds ratio
 oddsRatio <- function(case, control) {
-  # convert everything to be categorical
+  # convert to factor because data is categorical and discrete
   combo <- lapply(rbind(case, control), as.factor)
   #case <- lapply(case[, c("i401", "i362.0", "i411", "i410", "i428")], as.factor)
   #control <- lapply(control[, c("i401", "i362.0", "i411", "i410", "i428")], as.factor)
@@ -67,17 +68,14 @@ oddsRatio <- function(case, control) {
   exp(coef(logReg))
 }
 
-sum(simulateDiabeticCohort(100)[1, ])
-sum(simulateControlCohort(100)[1, ])
-a <- simulateDiabeticCohort(5)
-a[1:5, 1:5]
-colnames(a)
-class(colnames(a)[1:5])
+# see average number of conditions each person has
+sum(as.matrix(simulateDiabeticCohort(1000))) / 1000
+sum(as.matrix(simulateControlCohort(1000))) / 1000
+
 
 # If n < 50 ish there might not not be two categorical levels (either all 0s or all 1s but not both)
-# Output for n = 50: 48.9359008     1.1597954     3.6193148     6.6798753     0.5552008 
-oddsRatio(simulateDiabeticCohort(1000), simulateControlCohort(1000))
-
+# Output for n = 50: 4.893392e+09  6.750515e+08  7.247342e+08  1.139874e+09  1.697405e+09 
+oddsRatio(simulateDiabeticCohort(2500), simulateControlCohort(2500))
 
 # Always disconnect at the end
 dbDisconnect(con)
